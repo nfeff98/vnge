e72d7fc720a0cbf2aeb434e57f37f20a847888ee
pvh-code <nfeff98@gmail.com>
2025-10-27 23:17:48 -0400

    Update MirrorNode.ts

--- src/nodes/MirrorNode.ts
+++ src/nodes/MirrorNode.ts
@@ -28,46 +28,58 @@ export class MirrorNode extends BaseNode {
         };
     }
 
-    flipHorizontal(ctx: CanvasRenderingContext2D, inputCanvas: HTMLCanvasElement) {
-        if (!ctx || !inputCanvas) {
+    flipHorizontal(ctx: CanvasRenderingContext2D, inputElement: HTMLCanvasElement | HTMLVideoElement, width: number, height: number) {
+        if (!ctx || !inputElement) {
             return;
         }
-        ctx.clearRect(0, 0, inputCanvas.width, inputCanvas.height);
+        ctx.clearRect(0, 0, width, height);
         ctx.save();
         ctx.scale(-1, 1);
-        ctx.translate(-inputCanvas.width, 0);
-        ctx.drawImage(inputCanvas, 0, 0);
+        ctx.translate(-width, 0);
+        ctx.drawImage(inputElement, 0, 0);
         ctx.restore();
     }
 
-    flipVertical(ctx: CanvasRenderingContext2D, inputCanvas: HTMLCanvasElement) {
-        if (!ctx || !inputCanvas) {
+    flipVertical(ctx: CanvasRenderingContext2D, inputElement: HTMLCanvasElement | HTMLVideoElement, width: number, height: number) {
+        if (!ctx || !inputElement) {
             return;
         }
-        ctx.clearRect(0, 0, inputCanvas.width, inputCanvas.height);
+        ctx.clearRect(0, 0, width, height);
         ctx.save();
         ctx.scale(1, -1);
-        ctx.translate(0, -inputCanvas.height);
-        ctx.drawImage(inputCanvas, 0, 0);
+        ctx.translate(0, -height);
+        ctx.drawImage(inputElement, 0, 0);
         ctx.restore();
     }
 
     async executeInternal(): Promise<void> {
-        const inputCanvas = this.getInput('image');
-        if (!inputCanvas) {
+        const inputElement = this.getInput('image');
+        if (!inputElement) {
             return;
         }
 
-        const outputCanvas = this.createCanvas(inputCanvas.width, inputCanvas.height);
+        // Handle both HTMLVideoElement and HTMLCanvasElement inputs
+        let inputWidth: number, inputHeight: number;
+        if (inputElement instanceof HTMLVideoElement) {
+            inputWidth = inputElement.videoWidth;
+            inputHeight = inputElement.videoHeight;
+        } else if (inputElement instanceof HTMLCanvasElement) {
+            inputWidth = inputElement.width;
+            inputHeight = inputElement.height;
+        } else {
+            return;
+        }
+
+        const outputCanvas = this.createCanvas(inputWidth, inputHeight);
         const ctx = outputCanvas.getContext('2d');
         if (!ctx) {
             return;
         }
 
         if (this.getParameter('axis') === 'horizontal') {
-            this.flipHorizontal(ctx, inputCanvas);
+            this.flipHorizontal(ctx, inputElement, inputWidth, inputHeight);
         } else {
-            this.flipVertical(ctx, inputCanvas);
+            this.flipVertical(ctx, inputElement, inputWidth, inputHeight);
         }
 
         this.setOutput('image', outputCanvas);
